bb2963f48c463eecec2873d3ccc147820cf71027
Brooks Radtke <brooks@brooksradtke.com>
2021-04-09 10:26:01 -0700

    Push exercise to repo

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> unsorted = new List<int>();
+            List<int> sorted;
+
+            Random random = new Random();
+
+            Console.WriteLine("Original array elements: ");
+            for(int i = 0; i < 10; i++)
+            {
+                unsorted.Add(random.Next(0, 100));
+                Console.Write(unsorted[i] + " ");
+            }
+            Console.WriteLine();
+
+            sorted = MyMergeSort(unsorted);
+
+            Console.WriteLine("Sorted array elements: ");
+            foreach(int x in sorted)
+            {
+                Console.Write(x + " ");
+            }
+            Console.Write("\n");
+            Console.ReadLine();
+        }
+
+        private static List<int> MyMergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+            {
+                return unsorted;
+            }
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+            // Dividing the unsorted list
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MyMergeSort(left);
+            right = MyMergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    // Comparing the first 2 elements to see which is larger
+                    if (left.First() < right.First())
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+    }
+}
